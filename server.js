e1c8226f240c8ff560ecf2ec9e6efe4a5e169e21
MinhL311 <s4212400@rmit.edu.vn>
2026-08-18 20:01:10 +0700

    fix blog, blog-create and index

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
@@ -9,9 +10,14 @@ app.use(express.static('./'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// MOCK DATABASE 
 let users = [
     { id: 1, fullname: "Admin Test", username: "admin123", email: "admin@gmail.com", password: "password123", description: "I am the admin" }
 ];
+
+let blogs = []; 
+
+// ROUTES REGISTER
 
 app.get('/register', (req, res) => {
     res.render('register', { error: null }); 
@@ -36,6 +42,8 @@ app.post('/register', (req, res) => {
     res.redirect('/login');
 });
 
+// ROUTES LOGIN
+
 app.get('/login', (req, res) => {
     res.render('login', { error: null });
 });
@@ -56,6 +64,39 @@ app.post('/login', (req, res) => {
     res.redirect('/'); 
 });
 
+// ROUTES TRANG CHỦ & BLOG
+
+app.get('/', (req, res) => {
+    res.render('index', { blogs: blogs });
+});
+
+app.get('/blog', (req, res) => {
+    res.render('blog', { blogs: blogs });
+});
+
+app.get('/blog-create', (req, res) => {
+    res.render('blog-create');
+});
+
+app.post('/blog-create', (req, res) => {
+    const { title, author, date, category, tags, image, content } = req.body;
+
+    const newPost = {
+        id: Date.now(), 
+        title, 
+        author, 
+        date, 
+        category, 
+        tags,
+        content,
+        imageUrl: image || 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&fit=crop&w=600&q=80'
+    };
+
+    blogs.unshift(newPost);
+    console.log("=> Successfully created new blog:", title);
+
+    res.redirect('/blog');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
