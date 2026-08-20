835147a318e8c4c900c0070c5e1acfa5e0a55e98
MinhL311 <s4212400@rmit.edu.vn>
2026-08-20 15:23:10 +0700

    add demo for index

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,14 @@ const blogRouter = require('./blog');
 
 // ===== HOME PAGE ROUTE =====
 router.get('/', (req, res) => {
+    let activeBlogs = blogRouter.blogs || [];
+
+    if (activeBlogs.length === 0) {
+        activeBlogs = demoBlogs;
+    }
+
     res.render('index', {
-        blogs: blogRouter.blogs || [] 
+        blogs: activeBlogs 
     });
 });
 
